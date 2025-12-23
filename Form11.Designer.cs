9d5f4cc8a75b4515cecc950aac07a012af8cc79b
User <User@DESKTOP-J6QK3EQ>
2025-12-23 06:37:04 -0500

    sfdfsdf

--- Form11.Designer.cs
+++ Form11.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PROYECTO_INTEGRADOR
 {
-    partial class Form10
+    partial class Form11
     {
         /// <summary>
         /// Required designer variable.
@@ -28,38 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form10));
-            this.label1 = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form11));
+            this.info5titulo = new System.Windows.Forms.Label();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
-            // label1
+            // info5titulo
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(121, 27);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(249, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "TRATAMIENTO PARA EL HERPES";
+            this.info5titulo.AutoSize = true;
+            this.info5titulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.info5titulo.Location = new System.Drawing.Point(127, 35);
+            this.info5titulo.Name = "info5titulo";
+            this.info5titulo.Size = new System.Drawing.Size(218, 16);
+            this.info5titulo.TabIndex = 0;
+            this.info5titulo.Text = "TRATAMIENTO PARA EL VPH";
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(77, 75);
+            this.richTextBox1.Location = new System.Drawing.Point(114, 78);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(353, 96);
-            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Size = new System.Drawing.Size(306, 96);
+            this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
             // 
-            // Form10
+            // Form11
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(527, 250);
+            this.ClientSize = new System.Drawing.Size(504, 250);
             this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.label1);
-            this.Name = "Form10";
-            this.Text = "Form10";
+            this.Controls.Add(this.info5titulo);
+            this.Name = "Form11";
+            this.Text = "Form11";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -67,7 +67,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label info5titulo;
         private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
